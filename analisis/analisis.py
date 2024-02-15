d145ca426c6354cf3aefe6115a88b923e2cb29f1
tereesaalvarez <alvdeporteresa@gmail.com>
2024-02-15 17:08:15 +0100

    commit

--- analisis/analisis.py
+++ analisis/analisis.py
@@ -6,13 +6,10 @@ df = pd.read_csv('datos/champions_2016_2022.csv')
 # Divide la columna SEASON en dos partes usando el guion y toma el primer elemento como el año de inicio de la temporada
 df['SEASON'] = df['SEASON'].str.split('-').str[0]
 
-# Convierte la columna SEASON a formato de fecha
-df['SEASON'] = pd.to_datetime(df['SEASON'], format='%Y')
-
 # Itera sobre las temporadas y guarda cada subconjunto en un nuevo CSV
 for year in range(2016, 2022):
     # Filtra los datos para la temporada actual
-    current_season = df[(df['SEASON'] >= f'{year}') & (df['SEASON'] < f'{year+1}')]
+    current_season = df[df['SEASON'].str.contains(str(year))]
     
     # Guarda el subconjunto en un nuevo CSV
-    current_season.to_csv(f'champions_{year}-{year+1}.csv', index=False, sep=',', encoding='utf-8-sig')
+    current_season.to_csv(f'champions_{year}_{year+1}.csv', index=False, sep=',', encoding='utf-8-sig')
