42d2e989f00518230c93f4574a266ae9e83e6940
tereesaalvarez <alvdeporteresa@gmail.com>
2024-04-29 19:43:37 +0200

    arboles de decisión

--- arboldecision.py
+++ arboldecision.py
@@ -1,43 +1,43 @@
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error
 
 # Carga y preparación de datos
 def load_and_prepare_data(filepath):
     data = pd.read_csv(filepath)
-    # Supongamos que todas las columnas, excepto 'Porcentaje Victoria', son características
-    X = data.drop('Porcentaje Victoria', axis=1)
+    # Convertir las variables categóricas a numéricas usando One-Hot Encoding
+    data = pd.get_dummies(data)
+    X = data.drop('Porcentaje Victoria', axis=1)  # Asegúrate de que 'Porcentaje Victoria' es el nombre correcto
     y = data['Porcentaje Victoria']
     return X, y
 
-# Entrenamiento del modelo de árboles de decisión
+# Entrenamiento del modelo de regresión de árboles de decisión
 def train_decision_tree(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
-    model = DecisionTreeClassifier(random_state=42)
+    model = DecisionTreeRegressor(random_state=42)
     model.fit(X_train, y_train)
     return model, X_test, y_test
 
 # Evaluación del modelo
 def evaluate_model(model, X_test, y_test):
     predictions = model.predict(X_test)
-    accuracy = accuracy_score(y_test, predictions)
-    return accuracy, predictions
+    mse = mean_squared_error(y_test, predictions)
+    return mse, predictions
 
 # Display the standings based on the model's predictions
 def display_final_standings(predictions):
-    # Asignación de posiciones basada en predicciones
-    # Esto es solo un ejemplo simplificado, necesitarás ajustar esto basado en tus necesidades específicas
+    # Esta función deberá ajustarse si la salida es continua o de otro tipo no directamente ordinal
     standings = ["Bayern München", "Real Madrid", "Paris Saint-Germain", "Borussia Dortmund"]
     standings_sorted = sorted(zip(standings, predictions), key=lambda x: x[1], reverse=True)
     for position, (team, score) in enumerate(standings_sorted, 1):
-        print(f"{position}st Place: {team} - Score: {score}")
+        print(f"{position}st Place: {team} - Score: {score:.2f}")
 
 if __name__ == "__main__":
     filepath = 'datos_semis/combined_data.csv'
     X, y = load_and_prepare_data(filepath)
     model, X_test, y_test = train_decision_tree(X, y)
-    accuracy, predictions = evaluate_model(model, X_test, y_test)
+    mse, predictions = evaluate_model(model, X_test, y_test)
     
-    print(f"Model Accuracy: {accuracy}")
+    print(f"Model Mean Squared Error: {mse}")
     display_final_standings(predictions)
